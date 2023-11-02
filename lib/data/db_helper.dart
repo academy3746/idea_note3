48079ad082aa560d867bfec4e4bb04c63d9870bb
ijonghyeon <academy3746@gmail.com>
2023-11-02 17:52:17 +0900

    Build Database Handling Environment

---
+++ lib/data/db_helper.dart
@@ -0,0 +1,78 @@
+import 'package:idea_note3/data/db_config.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  late Database database;
+
+  /// Open DB Channel
+  Future<void> initDatabase() async {
+    String path = join(
+      await getDatabasesPath(),
+      "idea.db",
+    );
+
+    database = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database db, int version) {
+        db.execute("""
+          CREATE TABLE IF NOT EXISTS jh_write_idea (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            motive TEXT,
+            content TEXT,
+            importance INTEGER,
+            feedback TEXT,
+            dateTime INTEGER
+          )
+        """);
+      },
+    );
+  }
+
+  /// CREATE
+  Future<int> insertDatabase(IdeaInfo ideaInfo) async {
+    return await database.insert(
+      "jh_write_idea",
+      ideaInfo.toMap(),
+    );
+  }
+
+  /// READ
+  Future<List<IdeaInfo>> selectDatabase() async {
+    final List<Map<String, dynamic>> result =
+        await database.query("jh_write_idea");
+
+    return List.generate(
+      result.length,
+      (index) {
+        return IdeaInfo.fromMap(result[index]);
+      },
+    );
+  }
+
+  /// UPDATE
+  Future<int> updateDatabase(IdeaInfo ideaInfo) async {
+    return await database.update(
+      "jh_write_idea",
+      ideaInfo.toMap(),
+      where: "id = ?",
+      whereArgs: [ideaInfo.id],
+    );
+  }
+
+  /// DELETE
+  Future<int> deleteDatabase(int id) async {
+    return await database.delete(
+      "jh_write_idea",
+      where: "id = ?",
+      whereArgs: [id],
+    );
+  }
+
+  /// Close DB Channel
+  Future<void> closeDatabase() async {
+    await database.close();
+  }
+}
